d0b8e21e9b4f36d48db7b426f2bb5b40c82b65cf
aleksskorikov <aleksandrskorikov123@gmail.com>
2025-06-12 15:03:36 +0300

    add

--- src/app/admin/Orders/page.jsx
+++ src/app/admin/Orders/page.jsx
@@ -1,3 +1,5 @@
+"use client"; 
+
 import React, { useEffect, useState } from 'react';
 import styles from "./_orders.module.scss";
 import NewOrders from "./ordersComponents/NewOrders/newOrders.jsx";
